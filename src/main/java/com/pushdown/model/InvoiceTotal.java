6b33d34c35fdd9ee4000fb9b64a2540a84677330
colombefioren <colomberakotonjanahary@gmail.com>
2026-02-18 15:36:07 +0300

    feat: create invoice total class

---
+++ src/main/java/com/pushdown/model/InvoiceTotal.java
@@ -0,0 +1,63 @@
+package com.pushdown.model;
+
+import java.util.Objects;
+
+public class InvoiceTotal {
+    private Integer id;
+    private String customerName;
+    private Status status;
+
+    public InvoiceTotal() {
+    }
+
+    public InvoiceTotal(Integer id, String customerName, Status status) {
+        this.id = id;
+        this.customerName = customerName;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceTotal that = (InvoiceTotal) o;
+        return Objects.equals(id, that.id) && Objects.equals(customerName, that.customerName) && status == that.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customerName, status);
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceTotal{" +
+                "id=" + id +
+                ", customerName='" + customerName + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
---
+++ src/main/java/com/pushdown/model/Status.java
@@ -0,0 +1,7 @@
+package com.pushdown.model;
+
+public enum Status {
+    DRAFT,
+    CONFIRMED,
+    PAID
+}
