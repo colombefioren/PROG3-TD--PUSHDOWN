b1b62b64e8ce15b3fe0274e85be9c52cec7eee1c
colombefioren <colomberakotonjanahary@gmail.com>
2026-02-18 16:18:48 +0300

    feat: add sout tests in main

--- src/main/java/com/pushdown/Main.java
+++ src/main/java/com/pushdown/Main.java
@@ -1,7 +1,36 @@
 package com.pushdown;
 
+import com.pushdown.model.InvoiceStatusTotals;
+import com.pushdown.model.InvoiceTotal;
+import com.pushdown.repository.DataRetriever;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello");
-    }
+        DataRetriever dataRetriever = new DataRetriever();
+
+    System.out.println("===> findInvoiceTotals <===");
+
+    List<InvoiceTotal> invoiceTotals = dataRetriever.findInvoiceTotals();
+
+    System.out.println(invoiceTotals);
+
+    System.out.println("===> findConfirmedAndPaidInvoicesTotals <===");
+    List<InvoiceTotal> confirmedAndPaidInvoiceTotals = dataRetriever.findConfirmedAndPaidInvoiceTotals();
+
+    System.out.println(confirmedAndPaidInvoiceTotals);
+
+    System.out.println("===> computeStatusTotal <===");
+
+        InvoiceStatusTotals invoiceStatusTotals = dataRetriever.computeStatusTotals();
+
+        double totalPaid = invoiceStatusTotals.getPaidTotal();
+        double totalConfirmed = invoiceStatusTotals.getConfirmedTotal();
+        double totalDraft = invoiceStatusTotals.getDraftTotal();
+
+        System.out.println("total_paid = " + totalPaid);
+        System.out.println("total_confirmed = " + totalConfirmed);
+        System.out.println("total_draft = " + totalDraft);
+  }
 }
