4dd4ae121a70177bd99ff4182074edf7766309fe
colombefioren <colomberakotonjanahary@gmail.com>
2026-02-18 16:37:31 +0300

    refactor: format tests better

--- src/main/java/com/pushdown/Main.java
+++ src/main/java/com/pushdown/Main.java
@@ -10,18 +10,18 @@ public class Main {
     public static void main(String[] args){
         DataRetriever dataRetriever = new DataRetriever();
 
-    System.out.println("===> findInvoiceTotals <===");
+    System.out.println("\n===> findInvoiceTotals <===");
 
     List<InvoiceTotal> invoiceTotals = dataRetriever.findInvoiceTotals();
 
     System.out.println(invoiceTotals);
 
-    System.out.println("===> findConfirmedAndPaidInvoicesTotals <===");
+    System.out.println("\n===> findConfirmedAndPaidInvoicesTotals <===");
     List<InvoiceTotal> confirmedAndPaidInvoiceTotals = dataRetriever.findConfirmedAndPaidInvoiceTotals();
 
     System.out.println(confirmedAndPaidInvoiceTotals);
 
-    System.out.println("===> computeStatusTotal <===");
+    System.out.println("\n===> computeStatusTotal <===");
 
         InvoiceStatusTotals invoiceStatusTotals = dataRetriever.computeStatusTotals();
 
