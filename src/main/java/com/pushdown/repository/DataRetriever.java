e046ee4f1d98febc5ce54f8a6e5cf864f89f37e7
colombefioren <colomberakotonjanahary@gmail.com>
2026-02-18 15:43:01 +0300

    feat: add db connection attribute to data retriever

--- src/main/java/com/pushdown/repository/DataRetriever.java
+++ src/main/java/com/pushdown/repository/DataRetriever.java
@@ -1,13 +1,33 @@
 package com.pushdown.repository;
 
+import com.pushdown.db.DBConnection;
 import com.pushdown.model.InvoiceTotal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
 public class DataRetriever implements InvoiceRepository{
 
+    private final DBConnection dbConnection;
+    public DataRetriever() {
+        this.dbConnection = new DBConnection();
+    }
+
+
     @Override
     public List<InvoiceTotal> findInvoiceTotals() {
-        return List.of();
+       String sql = """
+""";
+
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    try{
+        conn = db
+    }
+
     }
 }
