52787e620bee284b5396a4d3239591850a3d4194
colombefioren <colomberakotonjanahary@gmail.com>
2026-02-19 12:55:18 +0300

    feat: add foreign key tax config in invoice table

--- sql/new_schema.sql
+++ sql/new_schema.sql
@@ -7,4 +7,7 @@ create table tax_config(
 insert into tax_config (label, rate) values
     ('TVA STANDARD',20);
 
+alter table invoice
+    add column tax_config_id int references tax_config(id);
+
 select setval('tax_config_id_seq',(select max(id) from tax_config));
